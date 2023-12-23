6fb37d6ae206c1dfbbafd37b8847362ec706c097
nuhlksmgl <76067783+nuhlksmgl@users.noreply.github.com>
2023-12-23 13:41:31 +0300

    gamejam
    firstcommit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private TextMeshProUGUI wordDisplay;
+    [SerializeField] private TextMeshProUGUI resultText;
+    [SerializeField] private float timeLimit = 15f;
+
+    private string currentWord;
+    private float currentTime;
+
+    private void Start()
+    {
+        StartNewTurn();
+    }
+
+    private void StartNewTurn()
+    {
+        // Yeni bir kelime al ve ekrana göster.
+        currentWord = GetRandomWord();
+        wordDisplay.text = currentWord;
+
+        // Zamaný sýfýrla.
+        currentTime = 0f;
+
+        // Input Field'ý temizle.
+        inputField.text = "";
+
+        // Sonuç yazýsýný temizle.
+        resultText.text = "";
+
+        // Input Field'a odaklan.
+        inputField.Select();
+        inputField.ActivateInputField();
+    }
+
+    private string GetRandomWord()
+    {
+        string[] words = { "oyun", "kelime", "þans", "harf", "bilgisayar" };
+        return words[Random.Range(0, words.Length)];
+    }
+
+    private void Update()
+    {
+        // Zamaný takip et ve süre dolduðunda yeni turu baþlat.
+        currentTime += Time.deltaTime;
+        if (currentTime >= timeLimit)
+        {
+            StartNewTurn();
+        }
+
+        // Enter tuþuna basýldýðýnda kullanýcýnýn girdiðini kontrol et.
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CheckUserInput();
+        }
+    }
+
+    private void CheckUserInput()
+    {
+        // Kullanýcýnýn giriþini al.
+        string userInput = inputField.text.ToLower();
+
+        // Eðer giriþin ilk harfi doðruysa, yeni bir tur baþlat.
+        if (!string.IsNullOrEmpty(userInput) && userInput[0] == currentWord[currentWord.Length - 1])
+        {
+            resultText.text = "Doðru!";
+            StartNewTurn();
+        }
+        else if (!string.IsNullOrEmpty(userInput))
+        {
+            resultText.text = "Yanlýþ!";
+        }
+    }
+}
